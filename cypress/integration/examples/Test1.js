1199a5b0e057307f45bf9e4026f484bf5daaad13
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-20 00:04:30 +0530

    test 1

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,6 +10,7 @@ describe('My first Test Suite', function()
         cy.wait(2500);
         cy.get('.product:visible').should('have.length',4);
         cy.get('.products').find('.product').should('have.length', 4);
+        cy.get('products').find('product').eq(2).contains('ADD TO CART').click();
     });
 
     // it('My secondtest case', function(){
