eac6cf46c93357322d60ffe2ace1d63f7a8e2e72
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-19 10:03:41 +0530

    Initial commit for Cypress Automation

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,18 @@
+// cypress spec1
+/// <refrence types="Cypress" />
+
+describe('My first Test Suite', function()
+{
+    it('My firstTest case', function(){
+        // test step
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length',4);
+    });
+
+    // it('My secondtest case', function(){
+    //     // test step
+    // });
+}
+);
