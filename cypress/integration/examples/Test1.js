f0755fb80878a74b63aa79bf88a79620f9e5c671
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-19 22:48:58 +0530

    test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -7,7 +7,7 @@ describe('My first Test Suite', function()
         // test step
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
         cy.get('.search-keyword').type('ca');
-        cy.wait(2000);
+        cy.wait(2500);
         cy.get('.product:visible').should('have.length',4);
         cy.get('.products').find('.product').should('have.length', 4);
     });
