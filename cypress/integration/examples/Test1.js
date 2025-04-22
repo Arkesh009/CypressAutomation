386c6eca3a7c7957af56a3203a214861697f693e
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-22 22:53:09 +0530

    test 1

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,23 +1,36 @@
 // cypress spec1
 /// <refrence types="Cypress" />
 
-describe('My first Test Suite', function()
-{
-    it('My firstTest case', function(){
+describe('My first Test Suite', function () {
+    it('My firstTest case', function () {
         // test step
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
         cy.get('.search-keyword').type('ca');
         cy.wait(2500);
-        cy.get('.product:visible').should('have.length',4);
-        cy.get('.products').find('.product').should('have.length', 4);
+        cy.get('.product:visible').should('have.length', 4);
+        // perent child chaining
+        cy.get('.products').as('productlocator')
+        cy.get('@productlocator').find('.product').should('have.length', 4);
         cy.get(':nth-child(3) > .product-action > button').click();
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+        cy.get('@productlocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
+            console.log('sf');
+        });
 
-        cy.get('.products').find('.product').each(($e1, index, $list) => {
-            const textVeg=$e1.find('h4.product-name').text()
-            if(textVeg.includes('Cashews')){
-                cy.wrap($e1).find('button').click();
+        // running each loop to find the target (cachews)
+        cy.get('@productlocator').find('.product').each(($e1, index, $list) => {
+            const textVeg = $e1.find('h4.product-name').text() // assigning value of $e1 to the variable with each iteration
+            // check if textVeg == cashews
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($e1).find('button').click(); // find the button and click
             }
+        });
+
+        // assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text', 'GREENKART');
+
+        // to print in logs
+        cy.get('.brand').then(function(logoelement){
+            cy.log(logoelement.text());
         });
 
     });
