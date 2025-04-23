cd5738e54ab346791a24b9c3ce44f85207645710
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-23 22:42:03 +0530

    test 2

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,32 @@
+// cypress spec1
+/// <refrence types="Cypress" />
+
+describe('My Second Test Suite', function () {
+    it('My firstTest case', function () {
+        // test step
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2500);
+
+        // perent child chaining
+        cy.get('.products').as('productlocator')
+        // running each loop to find the target (cachews)
+        cy.get('@productlocator').find('.product').each(($e1, index, $list) => {
+            // assigning value of $e1 to the variable with each iteration
+            const textVeg = $e1.find('h4.product-name').text()
+            // check if textVeg == cashews
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($e1).find('button').click(); // find the button and click
+            }
+        });
+
+        cy.get('.cart-icon > img').click();
+        cy.contains('PROCEED TO CHECKOUT').click();
+        cy.contains("Place Order").click(); // Use a more specific selector
+
+
+    });
+
+    
+}
+);
