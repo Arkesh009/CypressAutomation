a39c747bbc455d5639fd91564eb58e987b499383
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-24 23:51:37 +0530

    test 3

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -1,8 +1,8 @@
 // cypress spec1
 /// <refrence types="Cypress" />
 
-describe('My 3rd test suite', function(){
-    it('First test case', function(){
+describe('My 3rd test suite', function () {
+    it('First test case', function () {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
         cy.get('#checkBoxOption1').check().should('be.checked')
     })
