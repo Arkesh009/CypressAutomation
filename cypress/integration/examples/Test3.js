24c80d5a169cad47ec9171f1cc2b713594b6db47
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-25 02:27:31 +0530

    test3

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -14,6 +14,22 @@ describe('My 3rd test suite', function () {
         cy.get('select').select('Option2').should('have.value', 'option2')
 
         // Dynamic Dropdown
-        
+        cy.get('#autocomplete').type('ind')
+        cy.get('.ui-menu-item div').each(($e1, index, $list) => {
+            if($e1.text() === "India"){
+                $e1.click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value', 'India')
+
+        // hide/show item
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+        // radio button
+        cy.get('[value="radio1"]').check().should('be.checked')
     })
 })
