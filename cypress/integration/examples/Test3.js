b7e127cce3b7814103ed0a7eaf23c0fde075a3b6
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-25 01:25:30 +0530

    test 3

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -4,6 +4,16 @@
 describe('My 3rd test suite', function () {
     it('First test case', function () {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-        cy.get('#checkBoxOption1').check().should('be.checked')
+
+        // CheckBoxes
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option2', 'option3']).should('be.checked')
+
+        // Static Dropdown
+        cy.get('select').select('Option2').should('have.value', 'option2')
+
+        // Dynamic Dropdown
+        
     })
 })
