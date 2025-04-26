35b324434d205c6943b543aa8069722b248a2683
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-26 22:19:26 +0530

    test 7

--- cypress/integration/examples/Test7.js
+++ cypress/integration/examples/Test7.js
@@ -1,9 +1,17 @@
-/// <refrence types="Cypress" />
-// Mouse hover events are not supported in cypress, instead use jquerry or force click
+/// <reference types="Cypress" />
+// Cypress IntelliSense support for autocompletion and documentation
+
+// Test suite for handling mouse hover events
+// Note: Cypress does not natively support mouse hover events; use jQuery or force click as a workaround
 describe('My 7th test suite', () => {
     it('Mouse hover test case', () => {
+        // Visit the practice page containing the mouse hover element
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        // Click on the "Top" option under the mouse hover menu using force click
         cy.contains('Top').click({force: true})
+
+        // Verify that the URL includes the word 'top' after clicking
         cy.url().should('include', 'top')
     })
 })
