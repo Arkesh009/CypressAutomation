186ebf027d207f7371e06818bde29399689b27db
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-07 23:55:53 +0530

    API test

--- cypress/integration/examples/APItest.js
+++ cypress/integration/examples/APItest.js
@@ -1,14 +1,22 @@
 /// <reference types="cypress" />
 
+// Test suite for API testing
 describe('API testing suite', () => {
+
+    // Test case for sending a POST request to add a book
     it('First API test', function() {
+
+        // Send a POST request to the specified API endpoint with the request body
         cy.request('POST', 'http://216.10.245.166/Library/Addbook.php', {
-            "name": "Learn Appium Automation with Java",
-            "isbn": "bcggsss",
-            "aisle": "22s7",
-            "author": "John foe"
-        }).then(function(response){
+            "name": "Learn Appium Automation with Java", // Name of the book
+            "isbn": "bcggsss", // Unique identifier for the book
+            "aisle": "22s7", // Aisle number for the book
+            "author": "John foe" // Author of the book
+        }).then(function(response) {
+            // Assert that the response body contains the expected message
             expect(response.body).to.have.property("Msg", "successfully added")
+
+            // Assert that the response status code is 200 (OK)
             expect(response.status).to.eq(200)
         })
     })
