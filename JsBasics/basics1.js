3572643a9c5e9b9bea6a1d0fd790ca0f097f8345
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-13 22:23:36 +0530

    JS basics

---
+++ JsBasics/basics1.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+
+// Data types
+
+let a = 2; 
+
+let b = "Arkesh";
+
+let c = 99.8;
+
+let d = true;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+
+
+var e = "Bhargava";
+console.log(typeof(e));
+// we can redeclare a variable with var but not possible with let
+var e = 10;
+console.log(typeof(e));
