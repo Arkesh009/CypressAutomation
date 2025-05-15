8ed60818d3c05cb407f188f81530ab846ec5b799
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-15 23:59:39 +0530

    js

--- JsBasics/basics2.js
+++ JsBasics/basics2.js
@@ -1,21 +1,21 @@
 const flag = true;
 
 a = 10;
-while(a){
+while (a) {
     console.log(a);
     a--;
 }
 console.log("while loop ends\n");
 
-for(let i=0; i<10; i++){
-    console.log(i+1);
+for (let i = 0; i < 10; i++) {
+    console.log(i + 1);
 }
 console.log("for loop ends\n");
 
 let b = 0;
-while(b<10){
-    if(b==5){
-        
+while (b < 10) {
+    if (b == 5) {
+
         break;
     }
     console.log(b);
