80997583e85fbfb5777896801092af0139bc7f4c
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-22 00:01:26 +0530

    http mocking

--- cypress/integration/examples/fakeTest.js
+++ cypress/integration/examples/fakeTest.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 // Test suite for mocking HTTP responses in Cypress
-describe('Mocking HTTP respons', function(){
+describe('Mocking HTTP respons', function () {
     it('First HTTP Mocking', () => {
 
         // Visit the Angular app demo page
@@ -9,24 +9,23 @@ describe('Mocking HTTP respons', function(){
 
         // Intercept the GET request to the specified URL and mock the response
         cy.intercept({
-            method : 'GET',
-            url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+            method: 'GET',
+            url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
         },
-        {
-            statusCode : 200, // Mock a successful response
-            body : [{         // Mocked response body: an array with one book object
-                "book_name" : "RestAssured with Java",
-                "isbn" : "RSU",
-                "asile" : "2301"
-            }]
-        }).as('bookretrievals') // Alias this intercept for later reference
+            {
+                statusCode: 200, // Mock a successful response
+                body: [{         // Mocked response body: an array with one book object
+                    "book_name": "RestAssured with Java",
+                    "isbn": "RSU",
+                    "asile": "2301"
+                }]
+            }).as('bookretrievals') // Alias this intercept for later reference
 
         // Click the button to trigger the HTTP request
         cy.get("button[class='btn btn-primary']").click();
 
         // Wait for the intercepted request and validate the UI
-        cy.wait('@bookretrievals').then(({request, response}) => 
-        {
+        cy.wait('@bookretrievals').then(({ request, response }) => {
             // Assert that the number of table rows equals the number of books + 1 (header row)
             cy.get('tr').should('have.length', response.body.length + 1);
         })
